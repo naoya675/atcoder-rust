58fa05bbfbca408af64aa6f7388494dae186a789
naoya675 <64210874+naoya675@users.noreply.github.com>
2024-01-01 20:56:36 +0900

    add abc161

--- abc161/src/bin/d.rs
+++ abc161/src/bin/d.rs
@@ -1,3 +1,26 @@
+use proconio::input;
+use std::collections::VecDeque;
+
 fn main() {
-    todo!();
+    input! {
+        k: i32
+    }
+    let mut que: VecDeque<i64> = VecDeque::new();
+    for i in 1..10 {
+        que.push_back(i);
+    }
+    for _ in 1..k {
+        if let Some(q) = que.pop_front() {
+            if q % 10 != 0 {
+                que.push_back(q * 10 + (q % 10) - 1);
+            }
+            que.push_back(q * 10 + (q % 10));
+            if q % 10 != 9 {
+                que.push_back(q * 10 + (q % 10) + 1);
+            }
+        }
+    }
+    if let Some(q) = que.pop_front() {
+        println!("{}", q);
+    }
 }
