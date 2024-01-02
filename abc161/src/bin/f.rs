c5ef5df74d3e3e6ffe02319f47cf0e9f0d7aef4d
naoya675 <64210874+naoya675@users.noreply.github.com>
2024-01-02 20:08:06 +0900

    add abc161

--- abc161/src/bin/f.rs
+++ abc161/src/bin/f.rs
@@ -1,3 +1,38 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: i64
+    }
+    let mut pattern = divisors(n - 1).len() - 1;
+    for i in divisors(n) {
+        if i == 1 {
+            continue;
+        }
+        let mut n = n;
+        while n % i == 0 {
+            n /= i;
+        }
+        if n % i == 1 {
+            pattern += 1;
+        }
+    }
+    println!("{}", pattern);
+}
+
+fn divisors(n: i64) -> Vec<i64> {
+    let mut divisor = Vec::new();
+    let mut i = 1;
+    while i * i <= n {
+        if n % i == 0 {
+            if n / i == i {
+                divisor.push(i);
+            } else {
+                divisor.push(i);
+                divisor.push(n / i);
+            }
+        }
+        i += 1;
+    }
+    divisor
 }
