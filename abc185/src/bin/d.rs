13a2bb922aa004a23b483bf43342e9a727e52301
naoya675 <64210874+naoya675@users.noreply.github.com>
2024-01-07 20:26:51 +0900

    add abc185

--- abc185/src/bin/d.rs
+++ abc185/src/bin/d.rs
@@ -1,3 +1,29 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: i32,
+        m: i32,
+        mut a: [i32; m as usize]
+    }
+    if n == m {
+        println!("{}", 0);
+        return;
+    }
+    a.push(0);
+    a.push(n + 1);
+    a.sort();
+    let mut k = i32::MAX;
+    for a in a.windows(2) {
+        if a[1] - a[0] - 1 > 0 {
+            k = k.min(a[1] - a[0] - 1);
+        }
+    }
+    let mut res = 0;
+    for a in a.windows(2) {
+        if a[1] - a[0] - 1 > 0 {
+            res += (a[1] - a[0] - 1 + k - 1) / k;
+        }
+    }
+    println!("{}", res);
 }
